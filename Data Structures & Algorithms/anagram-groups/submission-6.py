3ae913a8c8b33b60906a43c8c6e446667302c05f
Vikram Magal <53961529+vkrmgl@users.noreply.github.com>
2026-03-30 15:33:45 -0400

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        grams = defaultdict(list)
+
+        for i in range(len(strs)):
+            word = [0]*26
+            for c in strs[i]:
+                word[ord(c)-ord('a')]+=1
+            grams[tuple(word)].append(strs[i])
+
+        return [x for x in grams.values()]
